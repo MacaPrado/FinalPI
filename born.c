fcd27066d586ea7e1ce8ff979bffe2ea81df679f
Micky <52582017+Mickyconca@users.noreply.github.com>
2019-12-03 11:51:56 -0300

    Update born.c

--- born.c
+++ born.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+	bornADT born= new();
 	FILE * f = fopen(argv[1], "r"); //abro el archivo de nacimientos que recibi como parametro con permisos de lectura
 
 	if(f == NULL){
@@ -18,6 +19,7 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+	readProvinces(f, born);
 	fclose(f); //cuando termino de leer todas las estaciones y agregarlas, cierro el archivo
 
 	f = fopen(argv[2], "r"); //abro el archivo de provincias 	//ES NECESARIO REPETIR EL CODIGO PORQUE SOLO PUEDO ABRIR UNO POR VEZ
@@ -27,8 +29,10 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+	readData(f, born);
 	fclose(f); //cuando termino de leer todas las provincias y agregarlas, cierro el archivo
 
+	//querys(born);
 	return 0;
 }
 
@@ -37,39 +41,39 @@ int main(int argc, char **argv){
 
 //______________________________________IDEA DE query1       NO LO PROBE CON FSANITIZE, ES UNA IDEA
 
-void query1(char *provinces[],long int bornByProvince[], const int dim){ //DONDE DIM ES EL NUMERO DE PROVINCIAS QUE DEBERIAMOS IR SUMANDO EN CADA ADD
-    FILE *fp;
-    fp=fopen("query1.csv", "w");				//ASI SE CREA EL ARCHIVO
-    fprintf(fp, "Provincia;Nacimientos\n");							//SI YA LOS ORDENAMOS ALFABETICAMENTE, ESE CODIGO PUEDE SER REUTILIZADO EN EL QUERY3 (QUE PIDE ORDEN POR PORCENTAJE Y ALFABETICO)
-    for(int i=0; i<dim; i++){
-        fprintf(fp, "%s;%ld\n", provinces[i],bornByProvince[i]);
-    }
-    fclose(fp);
-    return;
-}
-
-//______________________________________IDEA DE query2       NO LO PROBE CON FSANITIZE, ES UNA IDEA
-
-void query2(int *year, int *male, int *female, const int years){
-    FILE *fp;
-    fp=fopen("query2.csv", "w");
-    fprintf(fp, "Año;Varón;Mujer\n");
-    for(int i=0; i<years; i++){
-        fprintf(fp, "%d;%ld;%ld\n", year[i],male[i],female[i]);
-    }
-    fclose(fp);
-    return;
-}
-
-//______________________________________IDEA DE Query3       NO LO PROBE CON FSANITIZE, ES UNA IDEA
-
-void query3(char *provinces[],int percentage[], const int dim){		//TENDRIAMOS QUE PONERLE CONST A ESTOS VECTORES EN CADA QUERY?
-    FILE *fp;
-    fp=fopen("query3.csv", "w");
-    fprintf(fp, "Provincia;Porcentaje\n");
-    for(int i=0; i<dim; i++){
-        fprintf(fp, "%s;%ld%%\n", provinces[i],percentage[i]);
-    }
-    fclose(fp);
-    return;
-}
+// void query1(char *provinces[],long int bornByProvince[], const int dim){ //DONDE DIM ES EL NUMERO DE PROVINCIAS QUE DEBERIAMOS IR SUMANDO EN CADA ADD
+//     FILE *fp;
+//     fp=fopen("query1.csv", "w");				//ASI SE CREA EL ARCHIVO
+//     fprintf(fp, "Provincia;Nacimientos\n");							//SI YA LOS ORDENAMOS ALFABETICAMENTE, ESE CODIGO PUEDE SER REUTILIZADO EN EL QUERY3 (QUE PIDE ORDEN POR PORCENTAJE Y ALFABETICO)
+//     for(int i=0; i<dim; i++){
+//         fprintf(fp, "%s;%ld\n", provinces[i],bornByProvince[i]);
+//     }
+//     fclose(fp);
+//     return;
+// }
+//
+// //______________________________________IDEA DE query2       NO LO PROBE CON FSANITIZE, ES UNA IDEA
+//
+// void query2(int *year, int *male, int *female, const int years){
+//     FILE *fp;
+//     fp=fopen("query2.csv", "w");
+//     fprintf(fp, "Año;Varón;Mujer\n");
+//     for(int i=0; i<years; i++){
+//         fprintf(fp, "%d;%ld;%ld\n", year[i],male[i],female[i]);
+//     }
+//     fclose(fp);
+//     return;
+// }
+//
+// //______________________________________IDEA DE Query3       NO LO PROBE CON FSANITIZE, ES UNA IDEA
+//
+// void query3(char *provinces[],int percentage[], const int dim){		//TENDRIAMOS QUE PONERLE CONST A ESTOS VECTORES EN CADA QUERY?
+//     FILE *fp;
+//     fp=fopen("query3.csv", "w");
+//     fprintf(fp, "Provincia;Porcentaje\n");
+//     for(int i=0; i<dim; i++){
+//         fprintf(fp, "%s;%ld%%\n", provinces[i],percentage[i]);
+//     }
+//     fclose(fp);
+//     return;
+// }
