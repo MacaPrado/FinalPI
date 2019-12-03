68c31aa6906f3d554ac87dfd995fe4f2d80ee2b2
Micky <52582017+Mickyconca@users.noreply.github.com>
2019-12-03 11:29:38 -0300

    Update bornADT.h

--- bornADT.h
+++ bornADT.h
@@ -1,23 +1,10 @@
 #ifndef BORNADT_H_
 #define BORNADT_H_
-typedef struct transporteCDT * transporteADT;		//ESTO ES PARA TENER UNA IDEA, NO VA, HAY QUE EDITARLO TODO PARA NUESTRO TP
-typedef struct nodeLinea * pLinea;
-typedef struct nodeEstacion * pEstacion;
+typedef struct bornCDT * bornADT;		
+typedef struct nodeProv * provNode;
+typedef struct nodeFecha * fechaNode;
 
-typedef struct estacion{
-	char linea;
-	char* nombre;
-	int id;
-	long int diurnos;
-	long int nocturnos;
-}Testacion;
 
-typedef struct dia{
-	long int diurnos;
-	long int nocturnos;
-}tDia;								//HASTA ACA HAY QUE EDITAR
-
-//COMPARE NO SE PONIA ACA, NO?
 bornADT new(void);
 int isEmpty(const bornADT b);
 void toBegin(bornADT b);
