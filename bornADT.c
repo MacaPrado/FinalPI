28e9ea47b9c03322d8ad3778a2d4d6460abb4552
Micky <52582017+Mickyconca@users.noreply.github.com>
2019-12-03 12:12:38 -0300

    Update bornADT.c
    agregue readData (no anda todavia)

--- bornADT.c
+++ bornADT.c
@@ -49,8 +49,35 @@ int hasNext(bornADT b){
     return b->current != NULL;
 }
 
-void add(, ){
+void readData(FILE * f, bornADT b){
+  char buf[BLOQUE2]; //defino un vector de chars para poder usar en fgets
+    int cont=0;
+    int numCampo;
+    int year, sex;
+    long int persons;
 
+    while(fgets(buf, BLOQUE2, f)){
+        numCampo=0;
+        cont++;
 
+        if(cont==1)
+            continue; //me salteo el header
+        char * campo = strtok(buf, ","); //empiezo a recorrer la linea del CSV
+        while(campo){
+
+            if(numCampo == 0){
+                sscanf(campo, "%d", &year); 
+            }
+
+            if(numCampo == 3){ //uso el campo de "hasta" para corroborar si el pasajero es diurno o nocturno
+                sscanf(campo, "%d", &sex); //guardo la hora, descarto los minutos
+            }
+
+            campo = strtok(NULL, ","); //avanzo de campo
+            numCampo++;
+        }
+      
+        //addYears(b, year, sex, persons);
+    }
 }
 
