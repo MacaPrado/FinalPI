95698f7f9b282ab4da11d9c25f27cea6db3de8f4
Macarena Prado Torres <maca_pradot@hotmail.com>
2019-12-04 10:41:07 -0300

    agregue funciones addProvinces y addYears

--- bornADT.h
+++ bornADT.h
@@ -1,12 +1,13 @@
 #ifndef BORNADT_H_
 #define BORNADT_H_
 
-typedef struct bornCDT * bornADT;		
+typedef struct bornCDT * bornADT;
 typedef struct nodeProv * provNode;
 typedef struct nodeDate * dateNode;
 
 
 bornADT new(void);
+
 int isEmpty(const bornADT b);
 
 void toBegin(bornADT b);
@@ -14,5 +15,9 @@ void toBegin(bornADT b);
 int hasNext(bornADT b);
 
 void freeBorn(bornADT b);
+
+void addProvinces(bornADT born, provNode Province);
+
+void addYears(bornADT born, int year, int sex);
 
 #endif
